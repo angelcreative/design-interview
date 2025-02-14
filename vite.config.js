12bd3bf0babe8aea6e1a3bc5d5f6ba536fd57b09
angelcreative <ajsc50@gmail.com>
2025-02-14 23:41:53 +0100

    Corregida ruta en index.html

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/tweet-binder-analyzer/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
   build: {
     outDir: 'dist'
   }
