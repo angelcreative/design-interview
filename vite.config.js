725941509912a665665b59644270692ae2429173
angelcreative <ajsc50@gmail.com>
2025-02-15 00:26:08 +0100

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,5 @@ export default defineConfig({
   base: '/tweet-binder-analyzer/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
   }
 })
