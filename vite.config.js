740a580ba30252a01984d56f8d44e19c2b061a57
angelcreative <ajsc50@gmail.com>
2025-02-14 23:48:50 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tweet-binder-analyzer/',
+  base: '/tweet-binder-analyzer/', // Esto manejará las rutas en producción
   build: {
     outDir: 'dist'
   }
