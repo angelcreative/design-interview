182647a849d43d5f8cc19634d17cc8d5fbe203ee
angelcreative <ajsc50@gmail.com>
2025-02-14 23:53:34 +0100

    Corregidas rutas relativas

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tweet-binder-analyzer/', // Esto manejará las rutas en producción
+  base: './', // Cambiamos a ruta relativa
   build: {
     outDir: 'dist'
   }
